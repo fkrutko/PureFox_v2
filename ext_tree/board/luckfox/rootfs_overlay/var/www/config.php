2aa0214224b6d6a884bfab3ea5d18872fb7cc686
root <pavel.pogodin@gmail.com>
2026-06-29 20:35:14 +0200

    v2.5

--- ext_tree/board/luckfox/rootfs_overlay/var/www/config.php
+++ ext_tree/board/luckfox/rootfs_overlay/var/www/config.php
@@ -1,3 +1,3 @@
 <?php
-define('PUREFOX_VERSION', '2.4' . (file_exists('/opt/tidal.sqfs') ? '-test-dev' : ''));
+define('PUREFOX_VERSION', '2.5' . (file_exists('/opt/tidal.sqfs') ? '-test-dev' : ''));
 define('VERSION', PUREFOX_VERSION);
