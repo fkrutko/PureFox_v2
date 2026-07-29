c2d9df8da6454cbc853efb5e8116911e549042f1
hermes-ppydev <hermes@ppydev>
2026-07-29 18:31:07 +0200

    chore(webui): hide CelMusper on MAX

--- ext_tree/board/luckfox/rootfs_overlay/var/www/index.php
+++ ext_tree/board/luckfox/rootfs_overlay/var/www/index.php
@@ -39,7 +39,7 @@
     </div>
         <button class="btn-custom success" data-service="spotify" data-process="spotify">Spotify Connect</button>
         <button class="btn-custom success" data-service="qobuz" data-process="qobuz">Qobuz Connect</button>
-	<button class="btn-custom success" data-service="celmusper" data-process="celmusper">CelMusper</button>
+<!--	<button class="btn-custom success" data-service="celmusper" data-process="celmusper">CelMusper</button> -->
 	<?php if (file_exists('/opt/tidal.sqfs')): ?>
                     <button class="btn-custom success" data-service="tidalconnect" data-process="tidalconnect">Tidal Connect</button>
                     <?php endif; ?>
